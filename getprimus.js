56680cfd1d840d3f22ee19b6836c6cd4bf0988b7
Arnout Kazemier <info@3rd-Eden.com>
2015-05-22 12:57:20 -0700

    [refactor] Upgrading to latest

--- getprimus.js
+++ getprimus.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
+'use strict';
 
 var Workshopper = require('workshopper')
   , path = require('path');
 
-Workshopper({
-    name: 'getprimus'
-  , title: 'Learn Primus and tranform the real-time web'
-  , appDir: __dirname
-  , helpFile : path.join(__dirname, 'help.txt')
-  , menu: {
-      bg: 91
-    }
-}).init();
+var ws = new Workshopper({
+  name: 'getprimus',
+  title: 'Learn Primus and tranform the real-time web',
+  appDir: __dirname,
+  helpFile : path.join(__dirname, 'help.txt'),
+  menu: {
+    bg: 91 // HOT PURPLE BABE.
+  }
+});
